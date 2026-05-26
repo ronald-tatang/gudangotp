3cc9f5d95c2a2b0dd37b8cd3bee326b33c7f3350
ronald-tatang <ronaldtatang324@gmail.com>
2026-05-26 19:13:03 +0700

    Add files via upload

--- api/wallet.js
+++ api/wallet.js
@@ -9,7 +9,7 @@ const kv = new Redis({
   token: process.env.KV_REST_API_TOKEN,
 });
 
-const CASHI_API_KEY = process.env.CASHI_API_KEY || 'sk_0997fef664559ef4b97403c4354414ad';
+const CASHI_API_KEY = process.env.CASHI_API_KEY || 'CASHI-CFXQ470IQH5';
 const BASE_URL      = 'https://cashi.id/api';
 
 function cashiHeaders() {
